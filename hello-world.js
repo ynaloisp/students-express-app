4728e1c8b3723644f154722d93e511fc33f541b6
Ynalois Pangilinan <ynaloispangilinan@gmail.com>
2024-11-06 18:52:02 -0500

    added security/db fallback with pm2

--- hello-world.js
+++ hello-world.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/hello", (req, res) => {
   res.json({ message: "Hello World!" });
